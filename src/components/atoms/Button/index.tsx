e0feb5e23bd778a0d94792d13cbe359a4d1d6510
anpr <anpr@opinov8.com>
2022-10-13 16:55:16 -0400

    0.0.1-adding tests and precommit check

--- src/components/atoms/Button/index.tsx
+++ src/components/atoms/Button/index.tsx
@@ -3,6 +3,7 @@ import Button from './styles'
 export default (): JSX.Element => {
   return (
     <Button
+      data-testid="button"
       onClick={() => {
         console.log('Button was clicked')
       }}
